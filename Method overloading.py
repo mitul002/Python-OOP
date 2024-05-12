8bdcef9b88adf42ba542d68f68fa028542b52780
mitul002 <mitul15-4617@diu.edu.bd>
2024-05-12 09:09:54 +0600

    hello

--- Method overloading.py
+++ Method overloading.py
@@ -1,4 +1,4 @@
-class Calculator:
+'''class Calculator:
     def add(self, x, y=0, z=0):
         print(x+y+z)
 
@@ -11,3 +11,22 @@ result2.add(3, 4)
 
 result3 = Calculator()
 result3.add(3,4,5)
+'''
+
+
+'''class Calculator:
+    def add(self, x, y, z=0):
+        print(x + y + z)
+
+
+result = Calculator()
+result.add(5, 6)
+result.add(5, 6, 9)'''
+
+
+class Calculator:
+    def __init__(self,x,y,z=0):
+        print(x+y+z)
+
+result1= Calculator(2,6,)
+result= Calculator(2,7,2)
